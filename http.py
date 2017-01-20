bebd94685ae4fc02b1b1adb827d2c9ad88956a95
dennomwas <dennomwas@users.noreply.github.com>
2017-01-20 11:20:54 +0300

    getting url requests from your github account

--- http.py
+++ http.py
@@ -1,18 +1,6 @@
-import json
-import tweepy
-from tweepy import OAuthHandler
- 
-consumer_key = 'MmaN19kolXI40ZXmj13oOi4gz'
-consumer_secret = 'rzJo6Uu7FsEEGOCtwuA0PenawOWPDju2Lox8RrWxaTTIpMdUru'
-access_token = '75977508-70NFY9zJXCV2HPstR58mxzW6ihsUDRT8cpYZf3cvi'
-access_secret = 'W8DQNSIO29QfOfKA7nM9QlFAwguQN1o4hnCFWGms1USTk'
- 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
- 
-api = tweepy.API(auth)
+import requests
 
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    print(status.text) 
+req = requests.request('GET', 'http://api.github.com/users/dennomwas')
+print(req.json()) 
 
 
